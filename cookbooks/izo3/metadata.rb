9769ab7fb857c53504ef6337149df7219410be45
izo3 <hnganchev@gmail.com>
2012-06-11 19:39:48 +0000

    Add optional favorites

--- cookbooks/izo3/metadata.rb
+++ cookbooks/izo3/metadata.rb
@@ -16,4 +16,30 @@ attribute "about_me/first_name",
   :recipes => ["izo3::default"]
 
 
+attribute "about_me/favorite/food",
+  :required => "optional",
+  :display_name => "Favorite food",
+  :description => "Your favorite food",
+  :category    => "Favorites",
+  :default     => "curry",
+  :recipes     => ["izo3::default"]
+
+attribute "about_me/favorite/drink",
+  :required  => "optional",
+  :display_name => "Favorite drink",
+  :description  => "Your favorite drink",
+  :category     => "Favorites",
+  :default      => "Whiskey",
+  :recipes      => ["izo3::default"]
+
+attribute "about_me/favorite/hobby",
+   :required => "optional",
+   :display_name => "Hobby",
+   :description => "My favorite hobby.",
+   :category    => "Favorites",
+   :default     => "TV",
+   :recipes => ["izo3::default"]
+ 
+
+
 
