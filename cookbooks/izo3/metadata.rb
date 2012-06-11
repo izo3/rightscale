1d23b9e91d6ea1bb71b8d25ebaac2d6985d0e883
izo3 <hnganchev@gmail.com>
2012-06-11 18:09:52 +0000

    adding izo3 cookbook

---
+++ cookbooks/izo3/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Hristo Ganchev"
+maintainer_email "hnganchev@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures izo3"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "izo3::default","Prints Hello World!"
